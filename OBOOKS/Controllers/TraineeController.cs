523b4bc1d82ee173f3ad5dd33b610656ac2abf82
santhoshcade01 <santhoshcade01@gmail.com>
2024-04-02 15:19:01 +0530

    CodeGeneration instelled

--- OBOOKS/Controllers/TraineeController.cs
+++ OBOOKS/Controllers/TraineeController.cs
@@ -5,35 +5,44 @@ namespace OBOOKS.Controllers
     [Route("[contorller]")]
   public class TraineeController : Controller
   {
-    
+     private readonly ILogger<TraineeController> _logger;
+
+        public TraineeController(ILogger<TraineeController> logger)
+        {
+            _logger = logger;
+        }
     [Route("Trainee/Dashboard")]
   public IActionResult Dashboard(){
     return View("Dashboard","_LayoutTrainee");
 
   }
    public IActionResult EnrollinCourse(){
-    return View("Dashboard","_LayoutTrainee");
+    return View("EnrollinCourse","_LayoutTrainee");
 
   }
     public IActionResult MyCourses(){
-    return View("Dashboard","_LayoutTrainee");
+    return View("MyCourses","_LayoutTrainee");
 
   }
     public IActionResult ViewSessionsSchedule(){
-    return View("Dashboard","_LayoutTrainee");
+    return View("ViewSessionsSchedule","_LayoutTrainee");
 
   }
     public IActionResult Grades(){
-    return View("Dashboard","_LayoutTrainee");
+    return View("Grades","_LayoutTrainee");
 
   }
     public IActionResult Resources(){
-    return View("Dashboard","_LayoutTrainee");
+    return View("Resources","_LayoutTrainee");
 
   }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View("Error!");
+    }
   
-
-
   }
-
 }
+
